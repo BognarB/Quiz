b55f593852e835a19e815496a3fc4bc88c48ffc6
Renato Bognar <rbognar@progress.com>
2015-08-20 21:20:53 +0000

    Changes made by Renato Bognar (rbognar@progress.com).

--- QCon Quiz/dataListView/index.js
+++ QCon Quiz/dataListView/index.js
@@ -1,59 +1,101 @@
 'use strict';
 
 app.dataListView = kendo.observable({
-    onShow: function() {}
+    onShow: function () {},
+    afterShow: function () {}
 });
-(function(parent) {
-    var dataProvider = app.data.defaultProvider;
-    var processImage = function(img) {
-        if (!img) {
-            var empty1x1png = 'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVQI12NgYAAAAAMAASDVlMcAAAAASUVORK5CYII=';
-            img = 'data:image/png;base64,' + empty1x1png;
-        } else if (img.slice(0, 4) !== 'http' &&
-            img.slice(0, 2) !== '//' &&
-            img.slice(0, 5) !== 'data:') {
-            var setup = dataProvider.setup;
-            img = setup.scheme + ':' + setup.url + setup.apiKey + '/Files/' + img + '/Download';
+
+var provider = app.data.defaultProvider;
+(function (parent) {
+    var dataListViewModel = kendo.observable({
+        fields: {
+            Q1: '',
+            Q2: '',
+            Q3: '',
+            Q4: '',
+            Q5: '',
+            Q6: '',
+            Q7: '',
+            Q8: '',
+        },
+        submit: function () {
+            var userModel = new kendo.data.Model.define({
+                fields: {
+                    "FirstName": {
+                        type: 'string'
+                    },
+                    "LastName": {
+                        type: 'string'
+                    },
+                    "Company": {
+                        type: 'string'
+                    },
+                    "Q1": {
+                        type: 'string'
+                    },
+                    "Q2": {
+                        type: 'string'
+                    },
+                    "Q3": {
+                        type: 'string'
+                    },
+                    "Q4": {
+                        type: 'string'
+                    },
+                    "Q5": {
+                        type: 'string'
+                    },
+                    "Q6": {
+                        type: 'string'
+                    },
+                    "Q7": {
+                        type: 'string'
+                    },
+                    "Q8": {
+                        type: 'string'
+                    }
+                }
+            });
+            var url = "http://qcondemo-50158.onmodulus.net";
+            var quiz = new kendo.data.DataSource({
+                transport: {
+                    create: {
+                        url: url + '/user',
+                        type: 'POST'
+                    }
+                },
+                schema: {
+                    model: userModel,
+                },
+            });
+
+            var user
+            var success = function (data) {
+                user = {
+                    FirstName: data.result.Username,
+                    Email: data.result.Email,
+                    Q1: dataListViewModel.fields.Q1,
+                    Q2: dataListViewModel.fields.Q2,
+                    Q3: dataListViewModel.fields.Q3,
+                    Q4: dataListViewModel.fields.Q4,
+                    Q5: dataListViewModel.fields.Q5,
+                    Q6: dataListViewModel.fields.Q6,
+                    Q7: dataListViewModel.fields.Q7,
+                    Q8: dataListViewModel.fields.Q8
+                };
+                console.log(user);
+                quiz.add(new userModel(user));
+                quiz.sync();
+            }
+
+            provider.Users.currentUser()
+                .then(success, function (err) {
+                    alert(err);
+                });
+
+
         }
 
-        return img;
-    };
-
-    var dataSource = new kendo.data.DataSource({
-        type: 'everlive',
-        transport: {
-            typeName: 'Northwind',
-            dataProvider: dataProvider
-        },
-        schema: {
-            model: {
-                fields: {
-                    FirstName: {
-                        field: 'FirstName',
-                        defaultValue: ''
-                    },
-                    LastName: {
-                        field: 'LastName',
-                        defaultValue: ''
-                    },
-                }
-            }
-        },
     });
-
-    var dataListViewModel = kendo.observable({
-        dataSource: dataSource,
-        itemClick: function(e) {
-            app.mobileApp.navigate('#dataListView/details.html?uid=' + e.dataItem.uid);
-        },
-        detailsShow: function(e) {
-            var item = e.view.params.uid,
-                itemModel = dataSource.getByUid(item);
-            itemModel.photoUrl = processImage(itemModel.photo);
-            dataListViewModel.set('currentItem', itemModel);
-        },
-        currentItem: null
-    });
-
     parent.set('dataListViewModel', dataListViewModel);
 })(app.dataListView);
