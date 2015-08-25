58d6a1628042f5d4171fe4599e4e70fdd73bbb34
Renato Teixeira <rteixeir@progress.com>
2015-08-25 17:09:42 +0000

    Changes made by Renato Teixeira (rteixeir@progress.com).

---
+++ Progress Quiz/data/defaultProvider.js
@@ -0,0 +1,30 @@
+'use strict';
+
+(function () {
+    app.data.defaultProvider = new Everlive({
+        offlineStorage: true,
+        apiKey: '7RdaQT8Mej77w11z',
+        url: '//platform.telerik.com/bs-api/v1/',
+        scheme: 'https'
+    });
+
+
+    app.config = {};
+    app.data.defaultProvider.data('configuration').get()
+        .then(function (data) {
+            data.result.forEach(function (config) {
+                app.config[config.name] = config.value;
+            });
+
+            console.log(app.config);
+        });
+    document.addEventListener("online", function () {
+        app.data.defaultProvider.offline(false);
+        app.data.defaultProvider.sync();
+    });
+
+    document.addEventListener("offline", function () {
+        app.data.defaultProvider.offline(true);
+    });
+
+}());
