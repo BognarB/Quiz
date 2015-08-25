60bb50d2a8e86d3ee882c8f717027713a81ec257
Renato Teixeira <rteixeir@progress.com>
2015-08-25 17:18:09 +0000

    Changes made by Renato Teixeira (rteixeir@progress.com).

--- Progress Quiz/data/defaultProvider.js
+++ Progress Quiz/data/defaultProvider.js
@@ -7,6 +7,15 @@
         url: '//platform.telerik.com/bs-api/v1/',
         scheme: 'https'
     });
+    
+    app.config = {};
+    app.data.defaultProvider.data('configuration').get()
+        .then(function (data) {
+            data.result.forEach(function (config) {
+                app.config[config.name] = config.value;
+            });
+
+        });
 
     document.addEventListener("online", function() {
         app.data.defaultProvider.offline(false);
